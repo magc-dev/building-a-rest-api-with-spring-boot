a5c2e2b08a7e151549a119d6a08664826fa8b1e2
Miguel Ángel <magcarnota@gmail.com>
2023-07-03 20:04:06 +0200

    Implementing DELETE method and some tests.

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long>,
-        PagingAndSortingRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
     CashCard findByIdAndOwner(Long id, String owner);
     Page<CashCard> findByOwner(String owner, PageRequest amount);
+    boolean existsByIdAndOwner(Long id, String owner);
 }
